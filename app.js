e8959addcc9ffabbe4d9e5b15c2e43dbd4376b88
Snogdal2 <72727621+Snogdal2@users.noreply.github.com>
2023-09-21 15:15:45 +0200

    test

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors')
+const cors = require("cors");
 const port = 3000;
-const data = require('./sample4.json')
+const data = require("./sample4.json");
+const bodyParser = require("body-parser")
+const { writeFile } = require("fs");
+const path = "./sample4.json";
 
-app.use(cors())
+app.use(cors());
 
-app.get('/', (req, res) => {
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
+app.get("/", (req, res) => {
   res.send(data);
+});
+
+// app.post("/", function(req, res) {
+//   var num1 = Number(req.body.num1);
+//   var num2 = Number(req.body.num2);
+    
+//   var result = num1 + num2 ;
+    
+//   res.send("Addition - " + result);
+// });
+
+app.post("/", (req, res) => {
+  const purchase_hist = {
+    jens :[{
+     firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    gender: req.body.gender,
+    age: req.body.age,
+  number: req.body.number
+  }]
+  };
+  writeFile(path, JSON.stringify(purchase_hist, null, 2), (error) => {
+    if (error) {
+      console.log("An error has occurred ", error);
+      return;
+    }
+    console.log("Data written successfully to the file");
+  });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
